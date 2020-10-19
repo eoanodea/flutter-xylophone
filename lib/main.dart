a5597b17f34315b6a21f3b0a8dcf070399e817bf
Eoan O Dea <odeaeoan@gmail.com>
2020-10-19 19:10:34 +0200

    Installed audio library and created loop
    -Added in audioplayers library
    -Initialised as a static variable in the stateless class
    -Created an array and added in 7 items
    -Looped through each item and loaded a button, playing the corresponding note

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,12 +8,36 @@ void main() {
 }
 
 class XylophoneApp extends StatelessWidget {
+  /// Initialise audio cache
+  static AudioCache player = AudioCache();
+
   @override
   Widget build(BuildContext context) {
+    var noteArr = [];
+
+    /// play a note depending on it's file name
+    void playNote(num) {
+      player.play('note$num.wav');
+    }
+
+    /// Create an array between 1 and 7
+    for (var i = 0; i < 7; i++) {
+      noteArr.add(i + 1);
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: <Widget>[
+              for (var item in noteArr)
+                FlatButton(
+                  color: Color(Random().nextInt(0xffffffff)),
+                  onPressed: () => playNote(item),
+                  child: Text("Note $item"),
+                ),
+            ],
+          ),
         ),
       ),
     );
